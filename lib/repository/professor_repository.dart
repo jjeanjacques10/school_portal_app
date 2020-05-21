4fcbea2ea44d6f4888c1e12bd9b097978e5cc7ff
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-05-20 21:49:49 -0300

    Adicionando professor repository

---
+++ lib/repository/professor_repository.dart
@@ -0,0 +1,45 @@
+import 'package:school_portal_app/models/professor_model.dart';
+import 'package:school_portal_app/repository/database_helper.dart';
+
+class ProfessorRepository {
+  // Instancia do Database Helper
+  DatabaseHelper _databaseHelper;
+
+  // Construtor
+  ProfessorRepository() {
+    _databaseHelper = new DatabaseHelper();
+  }
+
+  Future<List<ProfessorModel>> findAll() async {
+    var connection = await _databaseHelper.connection;
+    var result = await connection.query(
+      "ProfessorModel",
+      columns: [
+        "rm",
+        "nome",
+        "senha",
+      ],
+    );
+
+    //Converte a lista de Maps para Lista de Cursos
+    List<ProfessorModel> listaProfessores = new List<ProfessorModel>();
+    for (Map i in result) {
+      listaProfessores.add(ProfessorModel.fromMap(i));
+    }
+
+    return listaProfessores;
+  }
+
+  Future<ProfessorModel> login(String rm, String senha) async {
+    var connection = await _databaseHelper.connection;
+    var sqlCommand =
+        "SELECT * FROM ProfessorModel WHERE rm = $rm and senha = $senha  ";
+
+    var results = await connection.rawQuery(sqlCommand);
+    if (results.length > 0) {
+      return new ProfessorModel.fromMap(results.first);
+    } else {
+      return null;
+    }
+  }
+}
