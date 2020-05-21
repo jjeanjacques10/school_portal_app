a94b3a1a79a9f694c0029bbe4428dac926fa8a00
gspetillo <gspetillo@gmail.com>
2020-05-20 22:00:12 -0300

    Modificações de layout na tela de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,9 +20,14 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.black,
-          title: Text('FIAP Professor', style: TextStyle(color: Colors.pink)),
+          title: Text('FIAPP',
+              style: TextStyle(
+                color: Colors.pink,
+                fontWeight: FontWeight.w500,
+              )),
           centerTitle: true,
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
@@ -32,71 +37,83 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 50,
+          ),
           child: SingleChildScrollView(
             child: Form(
               key: formKey,
               child: Center(
-                  child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: new InputDecoration(
-                      icon: const Icon(Icons.person_pin),
-                      fillColor: Colors.black,
-                      hintText: 'Digite o seu RM',
-                      labelText: 'RM',
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Portal do Professor",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.pink,
+                      ),
                     ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Digite o RM para logar';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {},
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: new InputDecoration(
-                      icon: const Icon(Icons.lock),
-                      fillColor: Colors.black,
-                      hintText: 'Digite sua senha',
-                      labelText: 'Senha',
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Digite a senha para logar';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {},
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: RaisedButton(
-                      child: Text("Entrar",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      color: Colors.pink,
-                      onPressed: () {
-                        if (formKey.currentState.validate()) {
-                          formKey.currentState.save();
-
-                          Navigator.pushNamed(context, '/home');
-                        } else {
-                          scaffoldKey.currentState.showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                'Não foi possível faze o login.',
-                              ),
-                            ),
-                          );
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          icon: const Icon(Icons.person_pin),
+                          fillColor: Colors.white,
+                          hintText: 'Digite o seu RM',
+                          labelText: "RM"),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Digite o RM para logar';
                         }
+                        return null;
                       },
+                      onSaved: (value) {},
                     ),
-                  ),
-                ],
-              )),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                        icon: const Icon(Icons.lock),
+                        fillColor: Colors.white,
+                        hintText: 'Digite sua senha',
+                        labelText: 'Senha',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Digite a senha para logar';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {},
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: RaisedButton(
+                        child: Text("Entrar",
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                        color: Colors.pink,
+                        onPressed: () {
+                          if (formKey.currentState.validate()) {
+                            formKey.currentState.save();
+
+                            Navigator.pushNamed(context, '/home');
+                          } else {
+                            scaffoldKey.currentState.showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Não foi possível faze o login.',
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
